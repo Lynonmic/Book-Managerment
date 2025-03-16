7f5baf00d930f26d38078bc40d7ca5f981fc7b23
Lynonmic <155310642+Lynonmic@users.noreply.github.com>
2025-03-17 01:27:36 +0700

    Delete frontend directory

--- frontend/lib/controllers/publisher_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:frontend/service/api_service.dart';
-
-class PublisherController {
-  // Hàm trả về danh sách publisher thay vì cập nhật state
-  Future<Map<String, dynamic>> fetchPublishers() async {
-    return await ApiService.getAllPublisher();
-  }
-}
