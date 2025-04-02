296e86b4433af903356872ed3756e94f7ad22e97
Lynonmic <nhinhinhi030303@gmail.com>
2025-04-02 08:04:39 +0700

    Update main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: const HomeScreen(),
+      home: const LoginPage(),
     );
   }
 }
